8942fcdc4c3b74fc9a7f684089b137543cb5f96f
Aiden Zegil <aidenzegil@gmail.com>
2023-12-11 17:26:11 -0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,69 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useRef, useState } from "react";
+import styles from "./page.module.css";
+import s from "./page.module.scss";
 
 export default function Home() {
+  const [panikClicks, setPanikClicks] = useState<number>(0);
+
+  const ref = useRef(null);
+
+  const buttonText = (() => {
+    switch (panikClicks) {
+      case 0:
+        return "Enter the secret garden";
+      case 1:
+        return "Stop";
+      case 2:
+        return "STOP";
+      case 3:
+        return "Please stop";
+      case 4:
+        return "PLEASE";
+      case 5:
+        return "OMG STOP";
+      case 6:
+        return "OMG PLEASE STOP";
+      case 7:
+        return "PLEAASEEE STAAAAAPPPP";
+      case 8:
+        return "😑";
+      case 9:
+        return "OMFG WHY PLEASE STOP";
+      default:
+        return "This button has not and will never do anything";
+    }
+  })();
+
+  const handleStopClick = () => {
+    if (panikClicks === 0) {
+      const audioElement = document.getElementById(
+        "scream"
+      ) as HTMLAudioElement;
+      document.body.addEventListener("mousemove", () => audioElement.play());
+    }
+
+    setPanikClicks(panikClicks + 1);
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button
+        className={panikClicks === 0 ? s.temptingButton : s.panikButton}
+        onClick={handleStopClick}
+      >
+        {buttonText}
+      </button>
+      <audio
+        ref={ref}
+        loop
+        id="scream"
+        src="/Hysterical_Goat.mp3"
+        style={{ display: "hidden" }}
+      >
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
     </main>
-  )
+  );
 }
