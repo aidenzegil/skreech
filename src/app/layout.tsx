4209a16f887bde9ab83d7518c3395a1a18242653
Aiden Zegil <aidenzegil@gmail.com>
2023-12-12 15:27:13 -0500

    adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1011363414869039"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
